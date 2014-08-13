bfd4a7c9004246d41d1bfa64f43571d0f91e3768
suraj.jh <suraj.jh@directi.com>
2014-08-13 20:45:46 +0530

    added user_home page

--- application/views/user_home.php
+++ application/views/user_home.php
@@ -28,7 +28,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?php echo base_url(); ?>/index.php/userHomeController/"><b><span class="brand_name">TwiMini</span></b></a>
+                    <a class="navbar-brand" href="<?php echo base_url(); ?>/index.php/userHomeController/"><b><span class="brand_name">TwiMini<i class="glyphicon glyphicon-home"></i></span></b></a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
@@ -80,26 +80,22 @@
             <div class="col-sm-3 col-md-3 col-lg-3">
                 <!-- ==============================USER BIO GOES HERE========================= -->
                 <div class="panel panel-default bio_panel">
-                    <div class="bio_panel_heading panel-heading " style="background-color: lightblue;"><span class="bio-title">Your Profile</span></div>
+                    <div class="bio_panel_heading panel-heading " style="background-color: lightblue;"><span class="bio-title">Your Profile&nbsp;<i class="glyphicon glyphicon-user"></i></span></div>
                     <div class="panel-body">
 
                         <!-- ===========DISPLAY PIC GOES HERE========= -->
                         <div class="row"> <img src="//placehold.it/200x200" class="img-responsive "style="width: 100%;" ></div>
                         <div class="row">
                             <!-- ========== BIO DETAILS GOES HERE =========== -->
-                            <pre>
-                              <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipi
-                                            Lorem ipsum dolor sit amet, consectetur adipic
-                                             Lorem ipsum dolor sit amet, consectetur adipi
-                                </p>
 
-                            </pre>
 
 
                         </div>
                     </div>
                 </div>
+
+                <button class="btn btn-primary btn-lg edit-profile-button">Edit Profile
+                    <i class="glyphicon glyphicon-edit"></i></button>
 
 
 
@@ -109,20 +105,37 @@
             <div class="col-sm-6 col-md-6 col-lg-6">
                 <!-- ==============================USER BIO GOES HERE========================= -->
                 <div class="panel panel-default news-feed">
-                    <div class=" panel-heading " style="background-color: lightblue;"><span class="news-feed-title">Your Profile</span></div>
+                    <div class=" panel-heading " style="background-color: lightblue;"><span class="news-feed-title">News Feeds&nbsp;&nbsp;<i class="glyphicon glyphicon-list-alt"></i></span></div>
                     <div class="panel-body">
 
                         <div class="tabbable">
                             <ul class="nav nav-tabs">
-                                <li class="active"><a href="#tab1" data-toggle="tab">Section 1</a></li>
-                                <li><a href="#tab2" data-toggle="tab">Section 2</a></li>
+                                <li class="active"><a href="#tab1" data-toggle="tab">Tweets&nbsp;&nbsp;<i class="glyphicon glyphicon-comment"></i></a></li>
+                                <li><a href="#tab2" data-toggle="tab">Following&nbsp;&nbsp;<i class="glyphicon glyphicon-cog"></i></a></li>
+                                <li><a href="#tab3" data-toggle="tab">Followers&nbsp;&nbsp;<i class="glyphicon glyphicon-cog"></i></a></li>
                             </ul>
                             <div class="tab-content">
                                 <div class="tab-pane active" id="tab1">
-                                    <p>I'm in Section 1.</p>
+                                    <p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                    <p>
+                                        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.
+                                    </p>
+                                    <p>
+                                        Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Vivamus facilisis diam at odio. Mauris dictum, nisi eget consequat elementum, lacus ligula molestie metus, non feugiat orci magna ac sem. Donec turpis. Donec vitae metus. Morbi tristique neque eu mauris. Quisque gravida ipsum non sapien. Proin turpis lacus, scelerisque vitae, elementum at, lobortis ac, quam. Aliquam dictum eleifend risus. In hac habitasse platea dictumst. Etiam sit amet diam. Suspendisse odio. Suspendisse nunc. In semper bibendum libero.
+                                    </p>
+                                    <p>
+                                        Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
+                                    </p>
+                                
                                 </div>
                                 <div class="tab-pane" id="tab2">
                                     <p>I'm in Section 2.</p>
+                                </div>
+                                <div class="tab-pane" id="tab3">
+                                    <p>I'm in Section 3.</p>
                                 </div>
                             </div>
                         </div>
@@ -137,7 +150,7 @@
             <div class="col-sm-3 col-md-3 col-lg-3">
                 <!-- ==============================USER BIO GOES HERE========================= -->
                 <div class="panel panel-default follow-panel">
-                    <div class=" panel-heading " style="background-color: lightblue;"><span class="follow-panel-heading">Follow These People.</span></div>
+                    <div class=" panel-heading " style="background-color: lightblue;"><span class="follow-panel-heading">Follow These People&nbsp;&nbsp;<i class="glyphicon glyphicon-globe"></i></span></div>
                     <div class="panel-body">
 
                         <!-- ===========DISPLAY PIC GOES HERE========= -->
