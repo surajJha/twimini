8c1b086c5079b7feb42aaebc21d72c22a55ad030
kirit.f <kirit.f@gmail.com>
2014-08-14 16:30:56 +0530

    XML Functionality Complete

--- libs/js/user_home.js
+++ libs/js/user_home.js
@@ -1,7 +1,58 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
+    var requestURL = 'http://localhost/twimini/index.php/userFeedController/getUserFeed';
+    console.log(requestURL);
+    var data = {'handle': 'suraj',
+        'count': 50,
+        'tid': 0};
+
+    $.ajax(
+            {
+                type: 'POST',
+                url: requestURL,
+                data: data,
+                dataType: 'json',
+                cache: false
+            }).done(function(msg, status, XHR) {
+        if (msg[0] === "success")
+        {                
+            for (var i = 0; i < msg[1].length; i++)
+            {
+                
+                $('.tweets').append('<div class="media tweet-object">' +
+                        '<a class="pull-left" href="#">' +
+                        '<img class="media-object" src="http://localhost/twimini/libs/images/dp.jpg" alt="..." style="height: 60px;width: 60px;">' +
+                        '</a>' +
+                        '<div class="media-body">' +
+                        '<h4 class="media-heading">@' + msg[1][i].handle + '</h4>' +
+                        msg[1][i].tweet +
+                        '...' +
+                        ' </div>' +
+                        '</div>');
+            }
+
+        }
 
 
+
+
+
+
+    }).fail(function(XHR, status, response) {
+        console.log(response);
+    });
+});
+
+
+/*< div class = "media tweet-object" >
+        < a class = "pull-left" href = "#" >
+        < img class = "media-object" src = "<?php echo base_url(); ?>libs/images/dp.jpg" alt = "..." style = "height: 60px;width: 60px;" >
+        < /a>
+        < div class = "media-body" >
+        < h4 class = "media-heading" > Media heading < /h4>
+        This is some sample text.This is some sample text.
+        This is some sample text.This is some sample text.
+        This is some sample text.This is some sample text.
+        This is some sample text.This is some sample text.
+        ...
+        < /div>
+        < /div>*/
